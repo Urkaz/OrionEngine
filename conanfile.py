decb4a16961ee5a354faa518a7f81bd59d276bac
Urkaz <mrurkaz@gmail.com>
2025-12-17 06:57:45 +0100

    Fix VCPKG and add Conan

---
+++ conanfile.py
@@ -0,0 +1,18 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain
+
+
+class CompressorRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        self.requires("spdlog/1.15.3")
+        self.requires("catch2/3.10.0")
+        self.requires("glfw/3.4")
+        self.requires("glm/1.0.1")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
