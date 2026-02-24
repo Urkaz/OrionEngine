c2b63860ca4effd1f59a1f739d666516a6b056d2
Urkaz <mrurkaz@gmail.com>
2026-02-24 06:40:34 +0100

    replaced std::bind in OE_BIND_EVENT_FN macro

--- src/OrionEngine/Orion/Core/Core.h
+++ src/OrionEngine/Orion/Core/Core.h
@@ -73,7 +73,8 @@ namespace Orion
 
 #define BIT(x) (1 << x)
 
-#define OE_BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+#define OE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) \
+{ return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Orion
 {
