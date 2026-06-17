8fb9f66eafb778caebcdc791eaa5e3218e6b6a75
Urkaz <mrurkaz@gmail.com>
2026-06-17 02:40:54 +0200

    Refactor LogLevel to use the namespace

--- src/OrionEngine/Orion/Core/Log.h
+++ src/OrionEngine/Orion/Core/Log.h
@@ -7,6 +7,8 @@
 
 #ifdef OE_ENABLE_LOGS
 
+#include <utility>
+
 namespace Orion
 {
     class Log
@@ -27,40 +29,37 @@ namespace Orion
         static Ref<spdlog::logger> s_ClientLogger;
     };
 } // namespace Orion
-#endif
 
-
-enum class OELogLevel
-{
-    Trace,
-    Info,
-    Warn,
-    Error,
-    Critical
-};
-
-#ifdef OE_ENABLE_LOGS
-    #include <utility>
 namespace Orion
 {
+
+    enum class LogLevel
+    {
+        Trace,
+        Info,
+        Warn,
+        Error,
+        Critical
+    };
+
     template <typename... Args>
-    inline void LogMessage(OELogLevel level, Ref<spdlog::logger>& logger, const std::string_view& fmt, Args&&... args)
+    inline void LogMessage(Orion::LogLevel level, Ref<spdlog::logger>& logger, const std::string_view& fmt, Args&&... args)
     {
         switch (level)
         {
-        case OELogLevel::Trace:
+        case Orion::LogLevel::Trace:
             logger->trace(spdlog::fmt_lib::runtime(fmt), std::forward<Args>(args)...);
             break;
-        case OELogLevel::Info:
+        case Orion::LogLevel::Info:
             logger->info(spdlog::fmt_lib::runtime(fmt), std::forward<Args>(args)...);
             break;
-        case OELogLevel::Warn:
+        case Orion::LogLevel::Warn:
             logger->warn(spdlog::fmt_lib::runtime(fmt), std::forward<Args>(args)...);
             break;
-        case OELogLevel::Error:
+        case Orion::LogLevel::Error:
             logger->error(spdlog::fmt_lib::runtime(fmt), std::forward<Args>(args)...);
             break;
-        case OELogLevel::Critical:
+        case Orion::LogLevel::Critical:
             logger->critical(spdlog::fmt_lib::runtime(fmt), std::forward<Args>(args)...);
             break;
         }
@@ -70,8 +69,8 @@ namespace Orion
 
 #ifdef OE_ENABLE_LOGS
     #define OE_INTERNAL_LOGGER(Level, Logger, ...) Orion::LogMessage(Level, Orion::Log::Logger(), __VA_ARGS__)
-    #define OE_CORE_LOG(Level, ...) OE_INTERNAL_LOGGER(OELogLevel::Level, GetCoreLogger, __VA_ARGS__)
-    #define OE_LOG(Level, ...) OE_INTERNAL_LOGGER(OELogLevel::Level, GetClientLogger, __VA_ARGS__)
+    #define OE_CORE_LOG(Level, ...) OE_INTERNAL_LOGGER(Orion::LogLevel::Level, GetCoreLogger, __VA_ARGS__)
+    #define OE_LOG(Level, ...) OE_INTERNAL_LOGGER(Orion::LogLevel::Level, GetClientLogger, __VA_ARGS__)
 #else
     #define OE_INTERNAL_LOGGER(Level, Logger, ...)
     #define OE_CORE_LOG(Level, ...)
